d3d28b5ac227879d4b5343e0456b94452c10f88a
Felipe Gonzalez <felipegonzalezurr@gmail.com>
2023-07-21 20:34:43 -0400

    btn guardar inicio

--- app/src/main/java/com/example/cl/ejercicio18kotlinmodulo5/MainActivity.kt
+++ app/src/main/java/com/example/cl/ejercicio18kotlinmodulo5/MainActivity.kt
@@ -2,14 +2,23 @@ package com.example.cl.ejercicio18kotlinmodulo5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.cl.ejercicio18kotlinmodulo5.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding =ActivityMainBinding.inflate(layoutInflater)
+        binding =ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+
+        binding.btnGuardar.setOnClickListener(View.OnClickListener {
+            val entero = binding.inputEntero.text.toString().toInt()
+            val texto = binding.inputTexto.toString()
+            val decimal = binding.inputDecimal.toString().toDouble()
+            val switch:Boolean = binding.switch1.isChecked
+        })
     }
 }
